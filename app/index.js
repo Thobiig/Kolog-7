f9ce703832cdaa1d9f73e1199de2e0e413d2cf0f
Thobiig <thobiig@gmail.com>
2025-04-22 10:53:38 -0500

    Re inicialización

---
+++ app/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+//fix para __dirname   
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+//Server
+const app = express();
+app.set("port", 4000);
+app.listen(app.get("port"));
+console.log("Servidor corriendo en el puerto", app.get("port"));
+
+//Configuracion
+app.use(express.static(__dirname + "/public"));
+
+
+//Rutas
+app.get("/", (req, res) => res.sendFile(__dirname + "/pages/login.html"));
+app.get("/register", (req, res) => res.sendFile(__dirname + "/pages/register.html"));
